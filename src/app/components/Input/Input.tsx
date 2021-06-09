6b5a5906c2b7bd435715a621a787a9a4ae2f2542
Lidia Kovac <lidia.kovac1998@gmail.com>
2021-06-09 22:35:43 +0200

    adds input

--- src/app/components/Input/Input.tsx
+++ src/app/components/Input/Input.tsx
@@ -3,8 +3,13 @@ import "./Input.scss";
 interface InputProps {
     name: string;
     type: string | false;
+    value?: string
 }
 const Input:React.FunctionComponent<InputProps> = ({name, type})=> {
-    return (<input  type={type || "text"} placeholder={name} id={name}/>)
+    return (
+    <>
+    <input className={type==='file' ? "hide": ""} type={type || "text"} placeholder={name} id={name}/>
+    {type ==='file' && <label htmlFor={name}>CLICK</label>}
+    </>)
 }
 export default Input;
