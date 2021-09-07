c8f0d123e6784d232160e86e5fb9a6145e96d2f6
LidiaKovac <lidia.kovac1998@gmail.com>
2021-09-07 22:40:09 +0200

    adds generateOption function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { ImageRequestBody } from "../interfaces";
+import { ImageRequestBody, Option, OptionClass } from "../interfaces";
 const bcryptjs = require("bcryptjs")
 //CREATES A FORMDATA 
 export const generateFormData = (imageData:ImageRequestBody) => {
@@ -14,4 +14,12 @@ export const encryptPassword = async(password:string):Promise<String> => {
 
 export const calcModifier = (value:number):number => {
     return Math.floor((value - 10)/2)
+}
+export const generateOptions = (elements:Array<any>, valueName:string, displayName: string) => {
+    let options:Array<Option> = []
+    elements.forEach(opt => {
+        let newOpt = new OptionClass(opt[valueName], opt[displayName])
+        options.push(newOpt)
+    })
+    return options
 }
